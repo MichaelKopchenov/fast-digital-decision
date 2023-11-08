199f01e47a2245eabdea4af8af91bf5a4f4aef60
Michael Kopchenov <michaelkopchenov@yandex.ru>
2023-11-08 14:15:14 +0300

    refactor: добавить анимации

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -7,15 +7,27 @@ import {
 import './Hello.css';
 
 function Hello() {
+    // const span = document.querySelector('.hello__title-span');
+
+    function removeCursor () {
+        document.getElementById("title").style = "border-right: none";
+    }
+
+    function getSpanBlock () {
+        document.getElementById("span").style.display = "block";
+      }
+      
+      setTimeout(removeCursor, 1500);
+      setTimeout(getSpanBlock, 1500);
 
     return (
         <section className='hello' id='Hello'>
-            <h1 className='hello__title'>
+            <h1 id='title' className='hello__text hello__title'>
                 {HELLO_TEXT_TITLE}
-                <span className='hello__title-span'>
-                    {HELLO_TEXT_SPAN}
-                </span>
             </h1>
+            <span id='span' className='hello__text hello__span'>
+                    {HELLO_TEXT_SPAN}
+            </span>
             <p className='hello__subtitle'>
                 {HELLO_TEXT_SUBTITLE}
             </p>
